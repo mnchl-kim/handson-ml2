e03c61fc0ece10f7e171707688720e19134b1ecc
Mincheol Kim <mincheol.kim.1996@gmail.com>
2020-05-21 17:15:40 +0900

    chapter 04

--- 04_training_linear_models.py
+++ 04_training_linear_models.py
@@ -232,6 +232,181 @@ plt.show()
 
 
 #%%
+import numpy as np
+import numpy.random as rnd
+
+np.random.seed(42)
+
+
+#%%
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X ** 2 + X + 2 + np.random.randn(m, 1)
+
+
+#%%
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([-3, 3, 0, 10])
+plt.show()
+
+
+#%%
+from sklearn.preprocessing import PolynomialFeatures
+
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+
+X[0]
+X_poly[0]
+
+
+#%%
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
+
+
+#%%
+X_new = np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+
+plt.plot(X, y, "b.")
+plt.plot(X_new, y_new, "r-", linewidth=2, label="Predictions")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.legend(loc="upper left", fontsize=14)
+plt.axis([-3, 3, 0, 10])
+plt.show()
+
+
+#%%
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+for style, width, degree in (("g-", 1, 300), ("b--", 2, 2), ("r-+", 2, 1)):
+    polybig_features = PolynomialFeatures(degree=degree, include_bias=False)
+    std_scaler = StandardScaler()
+    lin_reg = LinearRegression()
+    polynomial_regression = Pipeline([("poly_features", polybig_features),
+                                      ("std_scaler", std_scaler),
+                                      ("lin_reg", lin_reg),
+                                      ])
+    polynomial_regression.fit(X, y)
+    y_newbig = polynomial_regression.predict(X_new)
+    plt.plot(X_new, y_newbig, style, label=str(degree), linewidth=width)
+
+plt.plot(X, y, "b.", linewidth=3)
+plt.legend(loc="upper left")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([-3, 3, 0, 10])
+plt.show()
+
+
+#%%
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=10)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+        val_errors.append(mean_squared_error(y_val, y_val_predict))
+
+    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
+    plt.legend(loc="upper right", fontsize=14)
+    plt.xlabel("Training set size", fontsize=14)
+    plt.ylabel("RMSE", fontsize=14)
+    plt.axis([0, 80, 0, 3])
+    plt.show()
+
+
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+
+
+
+#%%
+from sklearn.pipeline import Pipeline
+
+polynomial_regression = Pipeline([("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+                                  ("lin_reg", LinearRegression()),
+                                  ])
+
+plot_learning_curves(polynomial_regression, X, y)
+
+
+#%%
+np.random.seed(42)
+m = 20
+X = 3 * np.random.rand(m, 1)
+y = 1 + 0.5 * X + np.random.randn(m, 1) / 1.5
+X_new = np.linspace(0, 3, 100).reshape(100, 1)
+
+
+#%%
+from sklearn.linear_model import Ridge
+
+
+def plot_model(model_class, polynomial, alphas, **model_kargs):
+    for alpha, style in zip(alphas, ("b-", "g--", "r:")):
+        model = model_class(alpha, **model_kargs) if alpha > 0 else LinearRegression()
+        if polynomial:
+            model = Pipeline([("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+                              ("std_scaler", StandardScaler()),
+                              ("regul_reg", model),
+                              ])
+        model.fit(X, y)
+        y_new_regul = model.predict(X_new)
+        lw = 2 if alpha > 0 else 1
+        plt.plot(X_new, y_new_regul, style, linewidth=lw, label=r"$\alpha = {}$".format(alpha))
+    plt.plot(X, y, "b.", linewidth=3)
+    plt.legend(loc="upper left", fontsize=15)
+    plt.xlabel("$x_1$", fontsize=18)
+    plt.axis([0, 3, 0, 4])
+
+
+plt.figure(figsize=(8, 4))
+plt.subplot(121)
+plot_model(Ridge, polynomial=False, alphas=(0, 10, 100), random_state=42)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.subplot(122)
+plot_model(Ridge, polynomial=True, alphas=(0, 10**-5, 1), random_state=42)
+plt.show()
+
+
+#%%
+ridge_reg = Ridge(alpha=1, solver="cholesky", random_state=42)
+ridge_reg.fit(X, y)
+ridge_reg.predict([[1.5]])
+
+
+#%%
+ridge_reg = Ridge(alpha=1, solver="sag", random_state=42)
+ridge_reg.fit(X, y)
+ridge_reg.predict([[1.5]])
+
+
+#%%
+from sklearn.linear_model import SGDRegressor
+
+sgd_reg = SGDRegressor(penalty="l2", max_iter=1000, tol=1e-3, random_state=42)
+sgd_reg.fit(X, y.ravel())
+sgd_reg.predict([[1.5]])
+
+
+#%%
 
 
 
