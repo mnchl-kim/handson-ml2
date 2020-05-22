afeb3b0360dbc1c420e5fba2c6dd0b990927af2d
Mincheol Kim <mincheol.kim.1996@gmail.com>
2020-05-22 14:19:30 +0900

    chapter 04

--- 04_training_linear_models.py
+++ 04_training_linear_models.py
@@ -393,12 +393,6 @@ ridge_reg.predict([[1.5]])
 
 
 #%%
-ridge_reg = Ridge(alpha=1, solver="sag", random_state=42)
-ridge_reg.fit(X, y)
-ridge_reg.predict([[1.5]])
-
-
-#%%
 from sklearn.linear_model import SGDRegressor
 
 sgd_reg = SGDRegressor(penalty="l2", max_iter=1000, tol=1e-3, random_state=42)
@@ -407,13 +401,214 @@ sgd_reg.predict([[1.5]])
 
 
 #%%
+ridge_reg = Ridge(alpha=1, solver="sag", random_state=42)
+ridge_reg.fit(X, y)
+ridge_reg.predict([[1.5]])
 
 
+#%%
+from sklearn.linear_model import Lasso
+
+plt.figure(figsize=(8, 4))
+plt.subplot(121)
+plot_model(Lasso, polynomial=False, alphas=(0, 0.1, 1), random_state=42)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.subplot(122)
+plot_model(Lasso, polynomial=True, alphas=(0, 10**-7, 1), random_state=42)
+plt.show()
 
 
+#%%
+from sklearn.linear_model import Lasso
+
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+lasso_reg.predict([[1.5]])
 
 
+#%%
+from sklearn.linear_model import SGDRegressor
+
+sgd_reg = SGDRegressor(penalty="l1", max_iter=1000, tol=1e-3, random_state=42)
+sgd_reg.fit(X, y.ravel())
+sgd_reg.predict([[1.5]])
 
 
+#%%
+from sklearn.linear_model import ElasticNet
 
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5, random_state=42)
+elastic_net.fit(X, y)
+elastic_net.predict([[1.5]])
+
+
+#%%
+from sklearn.model_selection import train_test_split
+
+np.random.seed(42)
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 2 + X + 0.5 * X ** 2 + np.random.randn(m, 1)
+
+X_train, X_val, y_train, y_val = train_test_split(X[:50], y[:50].ravel(), test_size=0.5, random_state=10)
+
+
+#%%
+from sklearn.base import clone
+from sklearn.metrics import mean_squared_error
+
+poly_scaler = Pipeline([("poly_features", PolynomialFeatures(degree=90, include_bias=False)),
+                        ("std_scaler", StandardScaler()),
+                        ])
+
+X_train_poly_scaled = poly_scaler.fit_transform(X_train)
+X_val_poly_scaled = poly_scaler.transform(X_val)
+
+sgd_reg = SGDRegressor(max_iter=1, tol=-np.infty, warm_start=True,
+                       penalty=None, learning_rate="constant", eta0=0.0005, random_state=42)
+
+minimum_val_error = float("inf")
+best_epoch = None
+best_model = None
+for epoch in range(1000):
+    sgd_reg.fit(X_train_poly_scaled, y_train)
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    val_error = mean_squared_error(y_val, y_val_predict)
+    if val_error < minimum_val_error:
+        minimum_val_error = val_error
+        best_epoch = epoch
+        best_model = clone(sgd_reg)
+
+
+#%%
+sgd_reg = SGDRegressor(max_iter=1, tol=-np.infty, warm_start=True,
+                       penalty=None, learning_rate="constant", eta0=0.0005, random_state=42)
+
+n_epochs = 500
+train_errors, val_errors = [], []
+for epoch in range(n_epochs):
+    sgd_reg.fit(X_train_poly_scaled, y_train)
+    y_train_predict = sgd_reg.predict(X_train_poly_scaled)
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    train_errors.append(mean_squared_error(y_train, y_train_predict))
+    val_errors.append(mean_squared_error(y_val, y_val_predict))
+
+best_epoch = np.argmin(val_errors)
+best_val_rmse = np.sqrt(val_errors[best_epoch])
+
+plt.annotate("Best model",
+             xy=(best_epoch, best_val_rmse),
+             xytext=(best_epoch, best_val_rmse + 1),
+             ha="center",
+             arrowprops=dict(facecolor="black", shrink=0.05),
+             fontsize=16,
+             )
+
+plt.plot(np.sqrt(train_errors), "r--", linewidth=2, label="Training set")
+plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="Validation set")
+plt.plot([0, n_epochs], [best_val_rmse - 0.03, best_val_rmse - 0.03], "k:", linewidth=2)
+plt.legend(loc="upper right", fontsize=14)
+plt.xlabel("Epoch", fontsize=14)
+plt.ylabel("RMSE", fontsize=14)
+plt.show()
+
+
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+
+t1a, t1b, t2a, t2b = -1, 3, -1.5, 1.5
+
+t1s = np.linspace(t1a, t1b, 500)
+t2s = np.linspace(t2a, t2b, 500)
+t1, t2 = np.meshgrid(t1s, t2s)
+T = np.c_[t1.ravel(), t2.ravel()]
+Xr = np.array([[1, 1], [1, -1], [1, 0.5]])
+yr = 2 * Xr[:, :1] + 0.5 * Xr[:, 1:]
+
+J = (1 / len(Xr) * np.sum((T.dot(Xr.T) - yr.T) ** 2, axis=1)).reshape(t1.shape)
+
+N1 = np.linalg.norm(T, ord=1, axis=1).reshape(t1.shape)
+N2 = np.linalg.norm(T, ord=2, axis=1).reshape(t1.shape)
+
+t_min_idx = np.unravel_index(np.argmin(J), J.shape)
+t1_min, t2_min = t1[t_min_idx], t2[t_min_idx]
+
+t_init = np.array([[0.25], [-1]])
+
+
+#%%
+def bgd_path(theta, X, y, l1, l2, core=1, eta=0.05, n_iterations=200):
+    path = [theta]
+    for iteration in range(n_iterations):
+        gradients = core * 2 / len(X) * X.T.dot(X.dot(theta) - y) + l1 * np.sign(theta) + l2 * theta
+        theta = theta - eta * gradients
+        path.append(theta)
+    return np.array(path)
+
+
+fig, axes = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(10.1, 8))
+for i, N, l1, l2, title in ((0, N1, 2., 0, "Lasso"), (1, N2, 0, 2., "Ridge")):
+    JR = J + l1 * N1 + l2 * 0.5 * N2 ** 2
+
+    tr_min_idx = np.unravel_index(np.argmin(JR), JR.shape)
+    t1r_min, t2r_min = t1[tr_min_idx], t2[tr_min_idx]
+
+    levelsJ = (np.exp(np.linspace(0, 1, 20)) - 1) * (np.max(J) - np.min(J)) + np.min(J)
+    levelsJR = (np.exp(np.linspace(0, 1, 20)) - 1) * (np.max(JR) - np.min(JR)) + np.min(JR)
+    levelsN = np.linspace(0, np.max(N), 10)
+
+    path_J = bgd_path(t_init, Xr, yr, l1=0, l2=0)
+    path_JR = bgd_path(t_init, Xr, yr, l1, l2)
+    path_N = bgd_path(np.array([[2.0], [0.5]]), Xr, yr, np.sign(l1) / 3, np.sign(l2), core=0)
+
+    ax = axes[i, 0]
+    ax.grid(True)
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
+    ax.contourf(t1, t2, N / 2., levels=levelsN)
+    ax.plot(path_N[:, 0], path_N[:, 1], "y--")
+    ax.plot(0, 0, "ys")
+    ax.plot(t1_min, t2_min, "ys")
+    ax.set_title(r"$\ell_{}$ penalty".format(i + 1), fontsize=16)
+    ax.axis([t1a, t1b, t2a, t2b])
+    if i == 1:
+        ax.set_xlabel(r"$\theta_1$", fontsize=16)
+    ax.set_ylabel(r"$\theta_2$", fontsize=16, rotation=0)
+
+    ax = axes[i, 1]
+    ax.grid(True)
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
+    ax.contourf(t1, t2, JR, levels=levelsJR, alpha=0.9)
+    ax.plot(path_JR[:, 0], path_JR[:, 1], "w-o")
+    ax.plot(path_N[:, 0], path_N[:, 1], "y--")
+    ax.plot(0, 0, "ys")
+    ax.plot(t1_min, t2_min, "ys")
+    ax.plot(t1r_min, t2r_min, "rs")
+    ax.set_title(title, fontsize=16)
+    ax.axis([t1a, t1b, t2a, t2b])
+    if i == 1:
+        ax.set_xlabel(r"$\theta_1$", fontsize=16)
+
+plt.show()
+
+
+#%%
+t = np.linspace(-10, 10, 100)
+sig = 1 / (1 + np.exp(-t))
+
+plt.figure(figsize=(9, 3))
+plt.plot([-10, 10], [0, 0], "k-")
+plt.plot([-10, 10], [0.5, 0.5], "k:")
+plt.plot([-10, 10], [1, 1], "k:")
+plt.plot([0, 0], [-1.1, 1.1], "k-")
+plt.plot(t, sig, "b-", linewidth=2, label=r"$\sigma(t) = \frac{1}{1 + e^{-t}}$")
+plt.xlabel("t")
+plt.legend(loc="upper left", fontsize=20)
+plt.axis([-10, 10, -0.1, 1.1])
+plt.show()
+
+
+#%%
 
