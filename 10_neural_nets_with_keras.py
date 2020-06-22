fbdc39aace1720239b95131a136364fc0036a9ad
Mincheol Kim <mincheol.kim.1996@gmail.com>
2020-06-22 17:58:56 +0900

    chapter 10

---
+++ 10_neural_nets_with_keras.py
@@ -0,0 +1,97 @@
+#%%
+import sys
+assert sys.version_info >= (3, 5)
+
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+import tensorflow as tf
+assert tf.__version__ >= "2.0"
+
+import numpy as np
+import os
+
+np.random.seed(42)
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc("axes", labelsize=14)
+mpl.rc("xtick", labelsize=12)
+mpl.rc("ytick", labelsize=12)
+
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "ann"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+import warnings
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
+
+
+#%%
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+
+iris = load_iris()
+X = iris.data[:, (2, 3)]
+y = (iris.target == 0).astype(np.int)
+
+per_clf = Perceptron(max_iter=1000, tol=1e-3, random_state=42)
+per_clf.fit(X, y)
+
+y_pred = per_clf.predict([[2, 0.5]])
+y_pred
+
+
+#%%
+a = - per_clf.coef_[0][0] / per_clf.coef_[0][1]
+b = - per_clf.intercept_ / per_clf.coef_[0][1]
+
+axes = [0, 5, 0, 2]
+
+x0, x1 = np.meshgrid(
+    np.linspace(axes[0], axes[1], 500).reshape(-1, 1),
+    np.linspace(axes[2], axes[3], 200).reshape(-1, 1),
+)
+
+X_new = np.c_[x0.ravel(), x1.ravel()]
+y_predict = per_clf.predict(X_new)
+zz = y_predict.reshape(x0.shape)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y==0, 0], X[y==0, 1], "bs", label="Not Iris-Setosa")
+plt.plot(X[y==1, 0], X[y==1, 1], "yo", label="Iris-Setosa")
+
+plt.plot([axes[0], axes[1]], [a * axes[0] + b, a * axes[1] + b], "k-", linewidth=3)
+from matplotlib.colors import ListedColormap
+custom_cmap = ListedColormap(['#9898ff', '#fafab0'])
+
+plt.contourf(x0, x1, zz, cmap=custom_cmap)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="lower right", fontsize=14)
+plt.axis(axes)
+
+save_fig("perceptron_iris_plot")
+plt.show()
+
+
+#%%
+
+
+
+
+
+
+
+
+
+
