de7797a8179c61d17874492fa0f2719346db325b
Mincheol Kim <mincheol.kim.1996@gmail.com>
2020-06-27 00:24:09 +0900

    chapter 10

--- 10_neural_nets_with_keras.py
+++ 10_neural_nets_with_keras.py
@@ -105,9 +105,9 @@ X_valid, X_train = (X_train_full[:5000] / 255.0).astype("float32"), (X_train_ful
 y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
 X_test = (X_test / 255.0).astype("float32")
 
-X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
-y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
-X_test = X_test / 255.0
+# X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
+# y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+# X_test = X_test / 255.0
 
 
 X_train.shape
@@ -191,10 +191,158 @@ model.compile(loss="sparse_categorical_crossentropy",
               optimizer="sgd",
               metrics=["accuracy"])
 
-history = model.fit(X_train, y_train, batch_size=32, epochs=5, validation_data=(X_valid, y_valid))
+history = model.fit(X_train, y_train, batch_size=32, epochs=30, validation_data=(X_valid, y_valid))
 
 
 #%%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+plt.show()
+
+
+#%%
+model.evaluate(X_test, y_test)
+
+
+#%%
+X_new = X_test[:3]
+y_proba = model.predict(X_new)
+y_proba.round(2)
+
+y_pred = model.predict_classes(X_new)
+y_pred
+
+np.array(class_name)[y_pred]
+
+y_new = y_test[:3]
+y_new
+
+
+#%%
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+housing = fetch_california_housing()
+
+X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_valid = scaler.transform(X_valid)
+X_test = scaler.transform(X_test)
+
+
+#%%
+np.random.seed(42)
+tf.random.set_seed(42)
+
+
+#%%
+model = keras.models.Sequential()
+model.add(keras.layers.Dense(30, activation="relu", input_shape=X_train.shape[1:]))
+model.add(keras.layers.Dense(1))
+
+
+#%%
+model.compile(loss="mean_squared_error", optimizer="sgd")
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
+
+
+#%%
+mse_test = model.evaluate(X_test, y_test)
+model.predict(X_test[:3])
+
+
+#%%
+plt.plot(pd.DataFrame(history.history))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+plt.show()
+
+
+#%%
+input_ = keras.layers.Input(shape=X_train.shape[1:])
+hidden1 = keras.layers.Dense(30, activation="relu")(input_)
+hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+concat = keras.layers.Concatenate()([input_, hidden2])
+# concat = keras.layers.concatenate([input_, hidden2])
+output = keras.layers.Dense(1)(concat)
+model = keras.Model(inputs=[input_], outputs=[output])
+
+model.summary()
+
+
+#%%
+model.compile(loss="mean_squared_error", optimizer=keras.optimizers.SGD(lr=1e-3))
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
+
+mse_test = model.evaluate(X_test, y_test)
+model.predict(X_test[:3])
+
+
+#%%
+input_A = keras.layers.Input(shape=[5], name="wide_input")
+input_B = keras.layers.Input(shape=[6], name="deep_input")
+hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
+hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+concat = keras.layers.Concatenate()([input_A, hidden2])
+output = keras.layers.Dense(1, name="output")(concat)
+model = keras.Model(inputs=[input_A, input_B], outputs=[output])
+
+
+#%%
+X_train_A, X_train_B = X_train[:, :5], X_train[:, 2:]
+X_valid_A, X_valid_B = X_valid[:, :5], X_valid[:, 2:]
+X_test_A, X_test_B = X_test[:, :5], X_test[:, 2:]
+X_new_A, X_new_B = X_test_A[:3], X_test_B[:3]
+
+model.compile(loss="mse", optimizaet=keras.optimizers.SGD(lr=1e-3))
+history = model.fit((X_train_A, X_train_B), y_train, epochs=20, validation_data=((X_valid_A, X_valid_B), y_valid))
+
+mse_test = model.evaluate((X_test_A, X_test_B), y_test)
+model.predict((X_new_A, X_new_B))
+
+
+#%%
+input_A = keras.layers.Input(shape=[5], name="wide_input")
+input_B = keras.layers.Input(shape=[6], name="deep_input")
+hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
+hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+concat = keras.layers.Concatenate()([input_A, hidden2])
+output = keras.layers.Dense(1, name="main_output")(concat)
+aux_output = keras.layers.Dense(1, name="aux_output")(hidden2)
+model = keras.Model(inputs=[input_A, input_B], outputs=[output, aux_output])
+
+
+#%%
+model.compile(loss=["mse", "mse"], loss_weights=[0.9, 0.1], optimizer=keras.optimizers.SGD(lr=1e-3))
+history = model.fit([X_train_A, X_train_B], [y_train, y_train], epochs=20,
+                    validation_data=([X_valid_A, X_valid_B], [y_valid, y_valid]))
+
+total_loss, main_loss, aux_loss = model.evaluate([X_test_A, X_test_B], [y_test, y_test])
+y_pred_main, y_pred_aux = model.predict([X_new_A, X_new_B])
+
+
+#%%
+class WideAndDeepModel(keras.models.Model):
+    def __init__(self, units=30, activation="relu", **kwargs):
+        super().__init__(**kwargs)
+        self.hidden1 = keras.layers.Dense(units, activation=activation)
+        self.hidden2 = keras.layers.Dense(units, activation=activation)
+
+
+
+
+
+
+
+
 
 
 
